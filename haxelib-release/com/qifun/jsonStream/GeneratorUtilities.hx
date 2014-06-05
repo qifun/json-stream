248269402bbd063bdd7463bed52bc6f5f1f55b3c
杨博 <pop.atry@gmail.com>
2014-06-05 10:54:54 +0800

    修复JsonBuilderFactory中的编译错误

--- haxelib-release/com/qifun/jsonStream/GeneratorUtilities.hx
+++ haxelib-release/com/qifun/jsonStream/GeneratorUtilities.hx
@@ -12,7 +12,7 @@ class GeneratorUtilities
   private static var VOID_COMPLEX_TYPE(default, never) =
     TPath({ name: "Void", pack: []});
 
-  private static var Dynamic_COMPLEX_TYPE(default, never) =
+  private static var DYNAMIC_COMPLEX_TYPE(default, never) =
     TPath({ name: "Dynamic", pack: []});
 
   private static function getFullName(module:String, name:String):String return
