3a709cf96a813dd15cbd641ac50e2460e61c093b
zxiy <zxiuyu@126.com>
2014-07-23 16:44:20 +0800

    将一些测试用例迁移至其他地方

--- src/test-haxe/SimpleEntities.hx
+++ src/test-haxe/SimpleEntities.hx
@@ -24,31 +24,3 @@ abstract SimpleAbstract(String)
   }
 }
 
-@:final
-class UserInfo 
-{
-	public function new() {}
-  public var hp:Int;
-  public var mp:Int;
-  public var skills:Array<Int> = [];
-}
-
-@:final
-class User
-{
-  public function new() {}
-	public var info:UserInfo = new UserInfo();
-}
-
-
-
-@:final
-class TypeTest
-{
-  public function new() {}
-  public var i:Int;
-  public var str:String;
-  public var f:Float;
-  public var bo:Bool;
- // public var nu:Void;
-}
