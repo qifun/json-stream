105951e31d6b18fad5dbb54d973bcd5143b2e920
杨博 <pop.atry@gmail.com>
2014-08-14 12:41:34 +0800

    修复编译错误

--- src/test-haxe/com/qifun/jsonStream/CrossPlatformTypeTest.hx
+++ src/test-haxe/com/qifun/jsonStream/CrossPlatformTypeTest.hx
@@ -4,22 +4,22 @@ import haxe.unit.TestCase;
 
 class CrossPlatformTypeTest extends TestCase
 {
- 
+
   function testType()
   {
     #if (java && scala && !scala_stm)
-      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.ArrayList<Dynamic>(scala.collection.immutable.Seq.SeqSingleton.MODULE.empty());
+      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.Vector<Dynamic>(scala.collection.immutable.Seq.SeqSingleton.MODULE.empty());
       var set = new com.qifun.jsonStream.crossPlatformTypes.Set<Dynamic>(scala.collection.immutable.Set.SetSingleton.MODULE.empty());
       var map = new com.qifun.jsonStream.crossPlatformTypes.Map<Dynamic, Dynamic>(scala.collection.immutable.Map.MapSingleton.MODULE.empty());
     #elseif cs
-      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.ArrayList<Dynamic>(new dotnet.system.collections.generic.List<Dynamic>());
+      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.Vector<Dynamic>(new dotnet.system.collections.generic.List<Dynamic>());
       var set = new com.qifun.jsonStream.crossPlatformTypes.Set<Dynamic>(new dotnet.system.collections.generic.HashSet<Dynamic>());
       var map = new com.qifun.jsonStream.crossPlatformTypes.Map<Dynamic, Dynamic>(new dotnet.system.collections.generic.Dictionary<Dynamic, Dynamic>());
     #elseif (scala_stm && java)
-      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.ArrayList<Dynamic>(scala.concurrent.stm.japi.STM.MODULE.newTArray(1).tarray());
+      var arrayList = new com.qifun.jsonStream.crossPlatformTypes.Vector<Dynamic>(scala.concurrent.stm.japi.STM.MODULE.newTArray(1).tarray());
       var set = new com.qifun.jsonStream.crossPlatformTypes.Set<Dynamic>(scala.concurrent.stm.japi.STM.MODULE.newTSet().tset());
       var map = new com.qifun.jsonStream.crossPlatformTypes.Map<Dynamic, Dynamic>(scala.concurrent.stm.japi.STM.MODULE.newTMap().tmap());
     #end
   }
-  
+
 }
