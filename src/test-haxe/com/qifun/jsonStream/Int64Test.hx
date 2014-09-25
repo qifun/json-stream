59ca369ef152a22867d38495e1a6aeea85f86ea8
zxiy <zxiuyu@126.com>
2014-09-25 19:11:59 +0800

    增加Int64相关测试用例

---
+++ src/test-haxe/com/qifun/jsonStream/Int64Test.hx
@@ -0,0 +1,41 @@
+package com.qifun.jsonStream;
+
+import haxe.unit.TestCase;
+import com.qifun.jsonStream.testUtil.JsonTestCase;
+import com.qifun.jsonStream.crossPlatformTypes.StmRef;
+import haxe.Int64;
+
+enum Int64Enum
+{
+  INT64(i:Int64);
+}
+
+class Int64Test extends JsonTestCase
+{
+  function testInt64InEnum()
+  {
+    var i64 = INT64(Int64.make(5, 5));
+    switch (i64)
+    {
+      case INT64(i):
+      {
+        assertMatch(5, Int64.getHigh(i));
+        assertMatch(5, Int64.getLow(i));
+      }
+    }
+  }
+
+/*
+  function testInt64AsRefParameter()
+  {
+    #if (scala && java)
+      var int64Stm:StmRef<Int64> = StmRef.empty();
+      assertDeepEquals(Int64.make(0, 0), int64Stm.underlying.single().get());
+    #else
+      var i64 = Int64.make(5, 5);
+      var int64Stm:StmRef<Int64> = new StmRef(i64);
+      assertDeepEquals(i64, int64Stm.underlying);
+    #end
+  }
+*/
+}
